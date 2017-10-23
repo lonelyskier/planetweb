0711ea2a410c74ae00486e820648c7cb7ca49499
Dennis <dennis_lundberg@yahoo.se>
2017-10-23 11:54:55 +0200

    Initial commit

---
+++ src/main/java/se/dl/planetweb/planetweb/PlanetwebApplication.java
@@ -0,0 +1,12 @@
+package se.dl.planetweb.planetweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanetwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanetwebApplication.class, args);
+	}
+}
